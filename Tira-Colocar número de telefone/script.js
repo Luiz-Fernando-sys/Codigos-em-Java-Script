e1ba0e8694ba6b19df10c82dee453504cf58fbcd
Luiz Fernando <66912726+Luiz-Fernando-sys@users.noreply.github.com>
2022-05-03 21:14:20 -0300

    Código em JavaScript para manipular DOM

---
+++ Tira-Colocar número de telefone/script.js
@@ -0,0 +1,13 @@
+document.getElementById('volta_numero').style.display = "none";
+
+function exibe_numero() {
+    document.getElementById('telefone').style.display = "block";
+    document.getElementById('com_numero').style.display = "none";
+    document.getElementById('volta_numero').style.display = "block";
+}
+
+function fecha_numero() {
+    document.getElementById('telefone').style.display = "none";
+    document.getElementById('com_numero').style.display = "block";
+    document.getElementById('volta_numero').style.display = "none";
+}
